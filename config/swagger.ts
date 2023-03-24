07de5034c7f65b033be13a013727645965518c21
Ryan Lester <ryan.bulot@stationfive.com>
2023-03-24 14:55:40 +0800

    added swagger docs in Auth controller

--- config/swagger.ts
+++ config/swagger.ts
@@ -1,33 +1,30 @@
 import { SwaggerConfig } from '@ioc:Adonis/Addons/Swagger'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default {
 	uiEnabled: true, //disable or enable swaggerUi route
 	uiUrl: 'docs', // url path to swaggerUI
 	specEnabled: true, //disable or enable swagger.json route
 	specUrl: '/swagger.json',
+	validatorUrl: null,
 
 	middleware: [], // middlewares array, for protect your swagger docs and spec endpoints
-
 	options: {
-		definition: {
-			openapi: '3.0.0',
-			info: {
-				title: 'Wookie books with swagger docs',
-				version: '1.0.0',
-			}
-		},
-		securitySchemes: {
-			bearerAuth: {
-			  	type: 'http',
-			  	scheme: 'bearer',
-			  	bearerFormat: 'JWT',
+		swaggerDefinition: {
+			securitySchemes: {
+				bearerAuth: {
+				type: 'http',
+				scheme: 'bearer',
+				bearerFormat: 'JWT'
+				}
 			},
-		},
-		securityDefinitions: {
-			bearerAuth: {
-			  	type: 'apiKey',
-			  	in: 'header',
-			  	name: 'Authorization',
+
+			securityDefinitions: {
+				bearerAuth: {
+				type: 'apiKey',
+				in: 'header',
+				name: 'Authorization'
+				},
 			},
 		},
 
